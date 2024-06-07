6f4a9bc4a363d52f1b39cb367eb20f77250d097d
Nelson Muriithi <nelsonmuriithi5@gmail.com>
2024-06-07 11:46:49 +0300

    setup cors

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from models.catalogue import Genre
 
 app = FastAPI()
+
+app.add_middleware(CORSMiddleware, allow_origins = ["*"], allow_credentials = True, allow_methods = ["*"], allow_headers = ["*"])
 
 class CatalogueModel(BaseModel):
     name: str
